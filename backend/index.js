f141b3e801e1266dfb8571a75983da237ab0ca5a
“aakriti <aakritikhanna206@gmail.com>
2024-04-13 16:34:52 +0530

    eight

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,35 @@
-const path = require ('path');
-const express = require ('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require ('dotenv').config();
-const cors = require('cors');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
 const PORT = 4444;
+const corsConfig = {
+  origin: "*",
 
-app.use(express.urlencoded({extended:true}));
+  credential: true,
+
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.options("",cors(corsConfig));
+
+app.use(cors(corsConfig));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-    origin :process.env.CLIENT_URL
-}));
-app.use('/', require('./router/routes'));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use("/", require("./router/routes"));
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() =>{
-    app.listen(PORT, () =>{
-        console.log(`http://localhost:` + PORT);
-    }); 
-})
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`http://localhost:` + PORT);
+    });
+  })
+  .catch((err) => console.log(err));
